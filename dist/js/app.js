08fc020ee94ea02ad157b2f968b2c880239b0181
Robert Tayco <rob@roberttayco.com>
2015-05-10 20:53:56 -0700

    setup of firebase chat

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 blocItOff = angular.module('BlocItOff', ['ui.router', 'firebase']);
 
 blocItOff.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
@@ -15,3 +16,4 @@ blocItOff.config(['$stateProvider', '$locationProvider', function($stateProvider
 blocItOff.controller('Main.controller', ['$scope', function($scope) {
     console.log('main view loaded');
 }]);
+},{}]},{},[1]);
