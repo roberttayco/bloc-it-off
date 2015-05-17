791f41dfbfc829ff21a60c8ba93ac52ee522d8cd
Robert Tayco <rob@roberttayco.com>
2015-05-17 14:49:46 -0700

    set up firebaseArray to update backend with frontend changes

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,18 +2,28 @@
 blocItOff = angular.module('BlocItOff', ['ui.router', 'firebase']);
 
 blocItOff.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
+   $locationProvider.html5Mode(true);
 
-    //  Main view
-    $stateProvider.state('main', {
-        url: '/',
-        controller: 'Main.controller',
-        templateUrl: '/templates/home.html'
-    });
+   //  Main view
+   $stateProvider.state('main', {
+      url: '/',
+      controller: 'Main.controller',
+      templateUrl: '/templates/home.html'
+   });
 
 }]);
 
-blocItOff.controller('Main.controller', ['$scope', function($scope) {
-    console.log('main view loaded');
+blocItOff.controller('Main.controller', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+   var ref = new Firebase('https://crackling-heat-1971.firebaseio.com/tasks');
+
+   // create synced array
+   $scope.tasks = $firebaseArray(ref);
+
+   // add new items to array
+   $scope.addTask = function() {
+      $scope.tasks.$add({
+         text: $scope.newTaskText
+      });
+   };
 }]);
 },{}]},{},[1]);
