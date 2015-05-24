ed8481ce423e596c73130a52cd28a9d578193d6d
Robert Tayco <rob@roberttayco.com>
2015-05-24 15:22:16 -0700

    task list as an angular service; determining age of task

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,5 @@
+var ref = new Firebase('https://crackling-heat-1971.firebaseio.com/tasks');
+
 blocItOff = angular.module('BlocItOff', ['ui.router', 'firebase']);
 
 blocItOff.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
@@ -12,16 +14,53 @@ blocItOff.config(['$stateProvider', '$locationProvider', function($stateProvider
 
 }]);
 
-blocItOff.controller('Main.controller', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
-   var ref = new Firebase('https://crackling-heat-1971.firebaseio.com/tasks');
+// Trying to access the services created below in the Main controller
+blocItOff.controller('Main.controller', ['$scope', 'TaskList', 'Firebase', function($scope, $TaskList, $Firebase) {
+   // var ref = new Firebase('https://crackling-heat-1971.firebaseio.com/tasks');
+
+   // $scope.tasks = $firebaseArray(ref);
 
    // create synced array
-   $scope.tasks = $firebaseArray(ref);
+   // $scope.tasks = $firebaseArray(ref);
 
    // add new items to array
-   $scope.addTask = function() {
-      $scope.tasks.$add({
-         text: $scope.newTaskText
-      });
+   // $scope.addTask = function() {
+   //    $scope.newTaskDate = new Date();
+   //    console.log($scope.newTaskDate); // This is working
+   //    $scope.tasks.$add({
+   //       text: $scope.newTaskText,
+   //       age:  $scope.newTaskDate    // But this is not being added to each task in Firebase
+   //    });
+   // };
+
+}]);
+
+// Experimenting with making the Firebase DB a service that can be accessed by
+// all the app's controllers
+blocItOff.service('Firebase', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+   var ref = new Firebase('https://crackling-heat-1971.firebaseio.com/tasks');
+   $scope.tasks = $firebaseArray(ref);
+}])
+
+// Making the TaskList and the addTask function a service
+blocItOff.service('TaskList', ['$rootScope', 'Firebase', function($rootScope) {
+
+   // creating synced array
+   // $rootScope.tasks = $firebaseArray(ref);
+
+   var taskList = {
+      tasks: Firebase.tasks, // testing this method
+
+      addTask: function(text, age) {
+         $rootScope.newTaskDate = new Date();
+         console.log($rootScope.newTaskDate);
+         $rootScope.tasks.$add({
+            text: $rootScope.newTaskText,
+            age:  $rootScope.newTaskDate
+         });
+      }
    };
+
+   return taskList;
+
 }]);
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var ref = new Firebase('https://crackling-heat-1971.firebaseio.com/tasks');
+
 blocItOff = angular.module('BlocItOff', ['ui.router', 'firebase']);
 
 blocItOff.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
@@ -13,17 +15,55 @@ blocItOff.config(['$stateProvider', '$locationProvider', function($stateProvider
 
 }]);
 
-blocItOff.controller('Main.controller', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
-   var ref = new Firebase('https://crackling-heat-1971.firebaseio.com/tasks');
+// Trying to access the services created below in the Main controller
+blocItOff.controller('Main.controller', ['$scope', 'TaskList', 'Firebase', function($scope, $TaskList, $Firebase) {
+   // var ref = new Firebase('https://crackling-heat-1971.firebaseio.com/tasks');
+
+   // $scope.tasks = $firebaseArray(ref);
 
    // create synced array
-   $scope.tasks = $firebaseArray(ref);
+   // $scope.tasks = $firebaseArray(ref);
 
    // add new items to array
-   $scope.addTask = function() {
-      $scope.tasks.$add({
-         text: $scope.newTaskText
-      });
-   };
+   // $scope.addTask = function() {
+   //    $scope.newTaskDate = new Date();
+   //    console.log($scope.newTaskDate); // This is working
+   //    $scope.tasks.$add({
+   //       text: $scope.newTaskText,
+   //       age:  $scope.newTaskDate    // But this is not being added to each task in Firebase
+   //    });
+   // };
+
 }]);
+
+// Experimenting with making the Firebase DB a service that can be accessed by
+// all the app's controllers
+blocItOff.service('Firebase', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+   var ref = new Firebase('https://crackling-heat-1971.firebaseio.com/tasks');
+   $scope.tasks = $firebaseArray(ref);
+}])
+
+// Making the TaskList and the addTask function a service
+blocItOff.service('TaskList', ['$rootScope', 'Firebase', function($rootScope) {
+
+   // creating synced array
+   // $rootScope.tasks = $firebaseArray(ref);
+
+   var taskList = {
+      tasks: Firebase.tasks, // testing this method
+
+      addTask: function(text, age) {
+         $rootScope.newTaskDate = new Date();
+         console.log($rootScope.newTaskDate);
+         $rootScope.tasks.$add({
+            text: $rootScope.newTaskText,
+            age:  $rootScope.newTaskDate
+         });
+      }
+   };
+
+   return taskList;
+
+}]);
+
 },{}]},{},[1]);
